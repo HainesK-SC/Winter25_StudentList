e8f5aa2dabc07ee876074841729a7711d8556651
HainesK-SC <hainesk@sheridancollege.ca>
2025-01-13 17:36:55 -0500

    Removing test comment for reverting previous commit

--- src/studentexample/Student.java
+++ src/studentexample/Student.java
@@ -8,12 +8,6 @@ package studentexample;
  *
  * @author Kyle Haines
  */
-
-/**
- * 
- * This comment will be removed in a commit rollback
- */
-
 public class Student {
     private String name;
     private int sid;
